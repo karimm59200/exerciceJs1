731f0e6d9c72a66efd8144b74f828c0aabc0d4bb
karim <karim-megri@hotmail.fr>
2023-01-10 11:40:48 +0100

    exercice2

---
+++ exercice1/exercice2/main.js
@@ -0,0 +1,9 @@
+let nb1  =  parseInt(prompt( "nb1 = " ));
+let nb2 = parseInt(prompt( "nb2 = " ));
+
+let addition = nb1 + nb2;
+
+console.log("le résultat de l'addition de " + nb1 + "+" + nb2 +" est de " + addition+ ".")
+// console.log("le résultat de l'addition de " `${nb1}` "est de"+  )
+
+
