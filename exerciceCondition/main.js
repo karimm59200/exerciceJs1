bef3c7289add9e7a508155bcdeabc72d482109c1
karim <karim-megri@hotmail.fr>
2023-01-10 17:19:42 +0100

    exercice condition

--- exerciceCondition/main.js
+++ exerciceCondition/main.js
@@ -3,7 +3,7 @@ let email ="Karim-megri@hotmail.fr", mdp="Karim991";
 email = prompt("Veuillez entrer votre email.");
 mdp = prompt ("Veuillez entrer votre mot de passe.");
 
-if (email && mdp) {
+if (email ==="karim-megri@hotmail.fr" && mdp==="karim991") {
   alert("Welcome, bienvenue à vous ! ");
 } else {
   alert("Oups, il y a un problème ! ");
